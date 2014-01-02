a1b1fc0b6b353a12245b1624319acd858f00f681
Keith Armstrong <keith1015@gmail.com>
2014-01-02 02:57:33 -0500

    Laid out the basics for home/login page and registration page

---
+++ global.php
@@ -0,0 +1,41 @@
+<?php
+define(NAME, "AggieGrade");
+define(VERSION, "0.1 Beta");
+
+function heading($title = "NULL") {
+?>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="img/favicon.png">
+
+    <title><?=NAME?> :: <?=$title?></title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/signin.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+<?php
+}
+
+function footer() {
+?>
+	<div id="footer">
+        <footer>
+            <p><?=NAME?> - Created by <a href="http://github.com/solewolf">Keith Armstrong</a>, <a href="http://github.com/ndm4766">Nick Melynk</a>, and <a href="https://github.com/Tvkwoodlands">Thomas Klingshirn</a></p>
+            <p class="text-muted">Full source available on <a href='https://github.com/solewolf/AggieGrade/'>Github</a></p>
+        </footer>
+    </div>
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+<?php
+}
+?>
