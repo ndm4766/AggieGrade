b241acbf9edbd08ccababc18e3163300a694730f
Keith Armstrong <keith1015@gmail.com>
2014-01-02 13:18:19 -0500

    Organized code indentation

--- index.php
+++ index.php
@@ -3,25 +3,25 @@ require_once("global.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <?=heading("Home")?>
-  </head>
-  <body>
-    <div class="container">
-    <img id="logo" src="img/tamu.png">
-      <form class="form-signin" role="form" action="login.php" method="POST">
-        <h2 class="form-signin-heading">Login to AggieGrade</h2>
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Username" required autofocus>
+    <head>
+        <?=heading("Home")?>
+    </head>
+    <body>
+        <div class="container">
+            <?=logo()?>
+            <form class="form-signin" role="form" action="login.php" method="POST">
+                <h2 class="form-signin-heading">Login to AggieGrade</h2>
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Username" required autofocus>
+                </div>
+                <div class="form-group">
+                    <input type="password" class="form-control" placeholder="Password" required>
+                </div>
+                <div align='center'>
+                    <input type="submit" class="btn btn-primary" value="Login">&nbsp;<input type="button" onClick="window.location='register.php'" class="btn btn-success" value="Register &raquo">
+                </div>
+            </form>
         </div>
-        <div class="form-group">
-          <input type="password" class="form-control" placeholder="Password" required>
-        </div>
-        <div align='center'>
-          <input type="submit" class="btn btn-primary" value="Login">&nbsp;<input type="button" onClick="window.location='register.php'" class="btn btn-success" value="Register &raquo">
-        </div>
-      </form>
-    </div> <!-- /container -->
     <?=footer()?>
   </body>
 </html>
--- register.php
+++ register.php
@@ -3,29 +3,32 @@ require_once("global.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <?=heading("Register")?>
-  </head>
-  <body>
-    <div class="container">
-    <img id="logo" src="img/tamu.png">
-      <form class="form-signin" role="form" action="register.php" method="POST">
-        <h2 class="form-signin-heading">Register to AggieGrade</h2>
-        <div class="form-group">
-          <input type="text" class="form-control" name="uin" placeholder="UIN" required autofocus>
+    <head>
+        <?=heading("Register")?>
+    </head>
+    <body>
+        <div class="container">
+            <?=logo()?>
+            <form class="form-signin" role="form" action="register.php" method="POST">
+                <h2 class="form-signin-heading">Register to AggieGrade</h2>
+                <div class="form-group">
+                    <input type="text" class="form-control" name="uin" placeholder="UIN" maxlength="9" required autofocus>
+                </div>
+                <div class="form-group">
+                    <input type="text" class="form-control" name="key" placeholder="Enrollment Key" maxlength="5" required>
+                </div>
+                <div class="form-group">
+                    <input type="text" class="form-control" name="username" placeholder="Username" required>
+                </div>
+                <div class="form-group">
+                    <input type="password" class="form-control" name="password" placeholder="Password" required>
+                    <input type="password" class="form-control" name="verify" placeholder="Verify Password" required>
+                </div>
+                <div align='center'>
+                    <input type="button" class="btn btn-primary" onClick="window.location='index.php'" value="&laquo Home">&nbsp;<input type="submit" class="btn btn-success" value="Submit">
+                </div>
+            </form>
         </div>
-        <div class="form-group">
-          <input type="text" class="form-control" name="username" placeholder="Username" required>
-        </div>
-        <div class="form-group">
-          <input type="password" class="form-control" name="password" placeholder="Password" required>
-          <input type="password" class="form-control" name="verify" placeholder="Verify Password" required>
-        </div>
-        <div align='center'>
-          <input type="button" class="btn btn-primary" onClick="window.location='index.php'" value="&laquo Home">&nbsp;<input type="submit" class="btn btn-success" value="Submit">
-        </div>
-      </form>
-    </div> <!-- /container -->
     <?=footer()?>
   </body>
 </html>
